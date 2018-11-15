c884d1f3d4646a0986f3e1d1f0399a72c8de3a64
khoom1 <44562047+khoom1@users.noreply.github.com>
2018-11-15 19:54:01 +1100

    Add files via upload

--- Problem2/serial.py
+++ Problem2/serial.py
@@ -3,13 +3,13 @@ import numpy as np
 from copy import deepcopy
 from mygauss import gauss
 
-def quad_cost1(lamb,A1,b1):
+def agent1(lamb,A1,b1):
 	coef_vect = deepcopy(b1)
 	coef_vect[-1] = b1[-1] + lamb
 	v1 = gauss(np.concatenate((A1,coef_vect),axis=1))
 	return v1[-1]
 
-def quad_cost2(lamb,A2,b2):
+def agent2(lamb,A2,b2):
 	coef_vect = deepcopy(b2)
 	coef_vect[0] = b2[0] - lamb
 	v2 = gauss(np.concatenate((A2,coef_vect),axis=1))
@@ -22,8 +22,8 @@ def do_serial(max_iter,alpha,A1,A2,b1,b2,verbose=False):
 	
 	begin = time.time()
 	for i in range(max_iter):
-		xi1[i] = quad_cost1(lamb,A1,b1)
-		xi2[i] = quad_cost2(lamb,A2,b2)
+		xi1[i] = agent1(lamb,A1,b1)
+		xi2[i] = agent2(lamb,A2,b2)
 		lamb = lamb - alpha*(xi1[i]-xi2[i])
 	end = time.time()
 	
